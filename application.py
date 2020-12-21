8834d98d214821fd17c4741ad12d8b2f8d9cb173
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2020-12-21 12:41:16 -0500

    Added routes

--- application.py
+++ application.py
@@ -2,6 +2,29 @@ from flask import Flask, render_template, redirect
 
 app= Flask(__name__)
 
-@app.route("/")
+@app.route("/") # home page
 def index():
-    return render_template("index.html")
+    return render_template("coming-soon.html")
+    #return render_template("index.html")
+
+"""
+@app.route("/about-me")
+def about_me():
+    return render_template("about-me.html")
+
+@app.route("/my-collections")
+def my_collections():
+    return render_template("collections.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/shop")
+def shop():
+    return render_template("shop.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+"""
