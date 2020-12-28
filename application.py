0766bfbc7b8e7c2e28d30d51a1a1d27dcdacc23a
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2020-12-28 17:55:51 -0500

    Added About Me page

--- application.py
+++ application.py
@@ -4,21 +4,22 @@ app= Flask(__name__)
 
 @app.route("/") # home page
 def index():
-    return render_template("coming-soon.html")
-    #return render_template("index.html")
+    #return render_template("coming-soon.html")
+    return render_template("index.html")
 
-"""
 @app.route("/about-me")
 def about_me():
     return render_template("about-me.html")
-
+    
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+"""
 @app.route("/my-collections")
 def my_collections():
     return render_template("collections.html")
 
-@app.route("/gallery")
-def gallery():
-    return render_template("gallery.html")
+
 
 @app.route("/shop")
 def shop():
--- static/index.js
+++ static/index.js
@@ -7,8 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
     */
     function setContainerHeight(){
         let windowHeight = window.innerHeight;
-        if (windowHeight < 350){
-            windowHeight = 350; // smallest height elements will resize for
+        if (windowHeight < 450){
+            windowHeight = 450; // smallest height elements will resize for
         }
         let navHeight = navbar.clientHeight;
         let container = document.querySelector('.container');
@@ -22,24 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
     resize index elements based on window size
     */
     function resizeElements(){
-        /*let windowHeight = window.innerHeight;
-        if (windowHeight < 350){
-            windowHeight = 350; // smallest height elements will resize for
-        }
-        let navHeight = navbar.clientHeight;
-        let container = document.querySelector('.container');*/
         let cutoutContainer = document.getElementById('cutout-container');
-        let welcomeText = document.getElementById('home-text');
 
-        //let restOfWindow = windowHeight - navHeight;
-        let [, navHeight, containerHeight] = setContainerHeight();
-        let welcomeY = containerHeight /2 - navHeight*2;
-
-        //console.log('new Y: ' + welcomeY); // debugging purposes
-
-        welcomeText.style.marginTop = welcomeY + "px";
-        //container.style.height = restOfWindow + "px";
-        cutoutContainer.style.height = restOfWindow + "px";
+        let [, , containerHeight] = setContainerHeight();
+        cutoutContainer.style.height = containerHeight + "px";
     }
 
     window.addEventListener('resize', resizeElements);
