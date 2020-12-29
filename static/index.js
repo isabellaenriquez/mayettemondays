3d1b654b8a680228ca42e38e39ec6e9c39d7fbdd
Isabella Enriquez <45607721+riquezen@users.noreply.github.com>
2020-12-29 17:59:44 -0500

    Fixed window resizing from slightly larger than window size

--- static/index.js
+++ static/index.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     let navbar = document.getElementById('nav-bar');
+    const body = document.body;
     resizeElements();
 
     /*
     function to fill whole window
     */
     function setContainerHeight(){
+        let bodyStyle = getComputedStyle(body);
         let windowHeight = window.innerHeight;
         if (windowHeight < 450){
             windowHeight = 450; // smallest height elements will resize for
         }
         let navHeight = navbar.clientHeight;
         let container = document.querySelector('.container');
+        let verticalBodySpace = parseInt(bodyStyle.getPropertyValue('padding-top').match(/\d+/)) + parseInt(bodyStyle.getPropertyValue('padding-bottom').match(/\d+/)) + parseInt(bodyStyle.getPropertyValue('margin-top').match(/\d+/)) + parseInt(bodyStyle.getPropertyValue('margin-bottom').match(/\d+/));
+        //body.style.paddingTop + body.style.paddingBottom + body.style.marginTop + body.style.marginBottom;
+        console.log(verticalBodySpace);
 
-        let containerHeight = windowHeight - navHeight;
+        let containerHeight = windowHeight - (navHeight + verticalBodySpace);
         container.style.height = containerHeight + "px";
 
         return [windowHeight, navHeight, containerHeight];
